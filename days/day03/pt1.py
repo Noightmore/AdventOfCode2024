0ce27c016fa746175ba6974c6839d62a8e7c173d
john.doe <robert.hak@tul.cz>
2024-12-02 23:34:00 +0100

    pre format for day3

---
+++ days/day03/pt1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+def read_file(path: Path):
+    with open(path, 'r') as file:
+        return file.readlines()
+
+
+def day03_part1():
+    path_in = Path(__file__).parent / 'in.txt'
+    path_test = Path(__file__).parent / 'test.txt'
+
+    data_in = read_file(path_in)
+    data_test = read_file(path_in)
+
+
+if __name__ == '__main__':
+    day03_part1()
