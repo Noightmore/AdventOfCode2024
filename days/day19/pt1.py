4430d006e04d3605caa18e2277fc4c9838569ad5
john.doe <robert.hak@tul.cz>
2024-12-20 17:10:41 +0100

    day 19 done

--- days/day19/pt1.py
+++ days/day19/pt1.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 def read_input(file='test.txt'):
     needed_combinations = []
     with open(file, 'r') as f:
@@ -8,44 +11,52 @@ def read_input(file='test.txt'):
         for line in f:
             needed_combinations.append(line.strip())
 
-    return avaible_towels, needed_combinations
+    return tuple(avaible_towels), needed_combinations
 
 
+@lru_cache(maxsize=None)
 def can_be_arranged(towels, c):
-    cursor = 0
-    while cursor < len(c):
-       # check if any word from towels is a substring in c a starting at cursor
-        longest_match = 0
-        for t in towels:
-            if c[cursor:].startswith(t):
-                if len(t) > longest_match:
-                    longest_match = len(t)
-                continue
 
-            if t == towels[-1] and longest_match == 0:
-                return False
-        cursor += longest_match
-    return True
+    for i in range(0,len(c)):
+       # check if any word from towels is a substring in c a starting at cursor
+        for t in towels:
+            if c[i:] == t:
+                return True
+            if c[i:].startswith(t):
+                #print(f"matched {t} in {c[i:]}")
+                match = can_be_arranged(towels, c[len(t):])
+                if match:
+                    return True
+
+        return False
 
 
 def dfs_combinations(towels, comb):
+
     for c in comb:
         yield can_be_arranged(towels, c)
 
 
 
 def day19_pt1():
-    towels, comb = read_input('test.txt')
+    towels, comb = read_input('in.txt')
     print(towels)
     print(comb)
 
     possible_towels = dfs_combinations(towels, comb)
+    print(sum(possible_towels))
     #print(list(possible_towels))
 
     # get count of trues
-    print(sum(possible_towels))
+
 
     # only take towels that have true next to them
 
+def debug(c):
+    towels, comb = read_input('test.txt')
+    print(towels, c)
+    print(can_be_arranged(towels, c))
+
 if __name__ == '__main__':
     day19_pt1()
+    #debug('bwurrg')
--- days/day19/pt2.py
+++ days/day19/pt2.py
@@ -1,2 +1,64 @@
+from functools import lru_cache
+
+
+def read_input(file='test.txt'):
+    needed_combinations = []
+    with open(file, 'r') as f:
+        avaible_towels = f.readline().strip().split(', ')
+
+        _ = f.readline()
+
+        for line in f:
+            needed_combinations.append(line.strip())
+
+    return tuple(avaible_towels), needed_combinations
+
+
+@lru_cache(maxsize=None)
+def can_be_arranged(towels, c):
+
+    counter = 0
+    for i in range(0,len(c)):
+        # check if any word from towels is a substring in c a starting at cursor
+        for t in towels:
+            if c[i:] == t:
+                counter += 1
+            if c[i:].startswith(t):
+                #print(f"matched {t} in {c[i:]}")
+                counter += can_be_arranged(towels, c[len(t):])
+
+        return counter
+    return 0
+
+
+def dfs_combinations(towels, comb):
+
+    for c in comb:
+        yield can_be_arranged(towels, c)
+
+
+
+def day19_pt1():
+    towels, comb = read_input('in.txt')
+    print(towels)
+    print(comb)
+
+    possible_towels = dfs_combinations(towels, comb)
+    print(sum(possible_towels))
+    #print(list(possible_towels))
+
+    # get count of trues
+
+
+    # only take towels that have true next to them
+
+def debug(c):
+    towels, comb = read_input('test.txt')
+    print(towels, c)
+    print(can_be_arranged(towels, c))
+
+if __name__ == '__main__':
+    day19_pt1()
+    #debug('bwurrg')
 
 
